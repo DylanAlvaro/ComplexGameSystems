ab9c43b7b39d75539039afa60ce7500efb72a341
Dylan Alvaro <dylanalvaro495@gmail.com>
2023-05-15 07:08:01 +1000

    not sure the changes but i think bsp

---
+++ ComplexGames/Assets/Scripts/UtubeTutorialScripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UtubeTutorialScripts
+{
+    public abstract class Node
+    {
+
+        private List<Node> childrenNodeList;
+        
+        public List<Node> ChildrenNodeList  {
+            get => childrenNodeList; }
+        
+        private bool Visited { get; set; }
+        public Vector2Int BottomLeftAreaCorner { get; set; }
+        public Vector2Int BottomRightAreaCorner { get; set; }
+        public Vector2Int TopRightAreaCorner { get; set; }
+        public Vector2Int TopLeftAreaCorner { get; set; }
+
+        public Node Parent { get; set; }
+        
+        public int TreeLayerIndex { get; set; }
+
+        public Node(Node parentNode)
+        {
+            childrenNodeList = new List<Node>();
+            this.Parent = parentNode;
+            if (parentNode != null)
+            {
+                parentNode.AddChild(this);
+            }
+        }
+
+        public void AddChild(Node node)
+        {
+            childrenNodeList.Add(node);
+        }
+
+        public void RemoveChild(Node node)
+        {
+            childrenNodeList.Remove(node);
+        }
+    }
+}
